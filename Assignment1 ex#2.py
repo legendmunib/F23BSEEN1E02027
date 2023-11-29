c0c090d56641d29486180d3fc22715a7dee0c861
MUNEEB AHSAN <151728123+legendmunib@users.noreply.github.com>
2023-11-29 18:13:47 +0500

    Update Assignment1 ex#2.py

--- Assignment1 ex#2.py
+++ Assignment1 ex#2.py
@@ -1,3 +1,8 @@
-input = input("enter a string: ")
-output = input[::2]
-print("even characters: ", output)
+prev_number = 0
+current_number = 0
+current_sum = 0
+for i in range(0, 11):
+    current_number = i
+    prev_number = current_number - 1
+    current_sum = current_number + prev_number
+    print(f"Current Number {current_number} Previous Number {prev_number} Sum: {current_sum}")
