09c77728d9138ad60522e728316ab911b9306934
MUNEEB AHSAN <151728123+legendmunib@users.noreply.github.com>
2023-11-29 18:28:56 +0500

    Update and rename assignment1 ex#10.10.py to assignment1 ex#4(a).py

--- assignment1 ex#10.10.py
+++
@@ -1,9 +0,0 @@
-for i in range(1,11):
-    print(i, end="\t")
-print()
-print("__\n")
-
-for j in range (1,11):
-      for k in range(1,11):
-          print (j * k, end ="\t")
-      print("\n")
---
+++ assignment1 ex#4(a).py
@@ -0,0 +1,11 @@
+numbers_y = [10,20,30,40,10]
+
+print("giveen list: ",numbers_y)
+
+first_number=numbers_y[0]
+last_number=numbers_y[-1]
+
+if first_number==last_number:
+  print("The result is true")
+else:
+      print("The result is false ")
